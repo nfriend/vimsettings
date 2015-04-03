4dfd6d5e5136e1dea7c1d1838e12f0bb24cd537f
Nathan Friend <hello@nathanfriend.io>
2015-04-02 22:36:34 -0500

    instruction changes

--- install-symlinks-nix.sh
+++ install-symlinks-nix.sh
@@ -7,5 +7,7 @@ rm -f ~/_vimrc
 ln -s "$(pwd)/_vimrc" ~/_vimrc
 
 echo
-echo "Symlinks to _vimrc and .vim directory successfully created.  For OS X, be sure to install the Solarized Dark terminal theme by double clicking \"Solarized Dark.terminal\" and setting the theme as default via Preferences -> Profiles -> Solarized Dark and clicking on \"Default\"."
+echo "Symlinks to _vimrc and .vim directory successfully created."
+echo
+echo "For OS X, be sure to install the Solarized Dark terminal theme by double clicking \"Solarized Dark.terminal\" (from https://github.com/tomislav/osx-terminal.app-colors-solarized) and setting the theme as default via Preferences -> Profiles -> Solarized Dark and clicking on \"Default\"."
 echo
