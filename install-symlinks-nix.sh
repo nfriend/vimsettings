f371b89ed9a54e47074188f0838c56278643ace7
Nathan Friend <hello@nathanfriend.io>
2015-04-02 22:24:51 -0500

    added terminal theme for OS X and instructions in sh script

--- install-symlinks-nix.sh
+++ install-symlinks-nix.sh
@@ -5,3 +5,7 @@ ln -s "$(pwd)/.vim" ~/.vim
 
 rm -f ~/_vimrc
 ln -s "$(pwd)/_vimrc" ~/_vimrc
+
+echo
+echo "Symlinks to _vimrc and .vim directory successfully created.  For OS X, be sure to install the Solarized Dark terminal theme by double clicking \"Solarized Dark.terminal\" and setting the theme as default via Preferences -> Profiles -> Solarized Dark and clicking on \"Default\"."
+echo
